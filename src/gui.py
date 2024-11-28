afa4f07b9033bef98e474a3c5c242941e11b2842
Alireza Jalouli <154112092+itsalirezajalouli@users.noreply.github.com>
2024-11-28 10:48:16 +0330

    Now we know how to do it!

--- src/gui.py
+++ src/gui.py
@@ -1,1 +1,114 @@
-#   Main GUI setup and window management
+# Main GUI setup and window management
+
+# Imports
+from PyQt6.QtGui import QAction 
+from PyQt6.QtWidgets import QGridLayout, QHBoxLayout, QMainWindow, QVBoxLayout, QWidget
+from guiComponents import Color
+
+# Main Window Object
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setWindowTitle('DickSilent!')
+        self.setFixedSize(1300, 700)
+
+        # Menu Bar
+        menu = self.menuBar()
+        menu.setStyleSheet('''
+            background-color: #2c2f33;
+            color: #fff;
+        ''')
+        fileMenu = menu.addMenu('&File')
+        editMenu = menu.addMenu('&Edit')
+        loadFlowMenu = menu.addMenu('&Load Flow')
+        viewMenu = menu.addMenu('&View')
+        docMenu = menu.addMenu('&Docs')
+
+        # New Project Button
+        newButton = QAction('New Project', self)
+        newButton.setStatusTip('Make a new project file')
+        newButton.triggered.connect(self.makeProject)
+        fileMenu.addAction(newButton)
+        fileMenu.addSeparator()
+
+        # Save Project Button
+        saveProButton = QAction('Save Project', self)
+        saveProButton.setStatusTip('Save this project as a file')
+        saveProButton.triggered.connect(self.saveProject)
+        fileMenu.addAction(saveProButton)
+        fileMenu.addSeparator()
+
+        # Open Project Button
+        openProButton = QAction('Open Project', self)
+        openProButton.setStatusTip('Open previous project files')
+        openProButton.triggered.connect(self.openProject)
+        fileMenu.addAction(openProButton)
+        fileMenu.addSeparator()
+
+        # Open Example Button
+        openExButton = QAction('Open IEEE Example', self)
+        openExButton .setStatusTip('Open IEEE Example files')
+        openExButton.triggered.connect(self.openExample)
+        fileMenu.addAction(openExButton)
+        fileMenu.addSeparator()
+
+        # Exit Button
+        exitButton = QAction('Exit', self)
+        exitButton.setStatusTip('Exit the program')
+        exitButton.triggered.connect(self.exitProgram)
+        fileMenu.addAction(exitButton)
+        fileMenu.addSeparator()
+
+        # Run Gauss-Siedel Button
+        gsButton = QAction('Run Gauss-Siedel Method', self)
+        gsButton.setStatusTip('Run Gauss Seidel Method of Load Flow')
+        gsButton.triggered.connect(self.gsLoadFlow)
+        loadFlowMenu.addAction(gsButton)
+        loadFlowMenu.addSeparator()
+
+        # Run Newton-Raphson Button
+        nrButton = QAction('Run Newton-Raphson Method', self)
+        nrButton.setStatusTip('Run Newton Raphson Method of Load Flow')
+        nrButton.triggered.connect(self.nrLoadFlow)
+        loadFlowMenu.addAction(nrButton)
+        loadFlowMenu.addSeparator()
+
+        # Run Fast-Decoupled Button
+        fdButton = QAction('Run Fast-Decoupled Method', self)
+        fdButton.setStatusTip('Run Fast-Decoupled Method of Load Flow')
+        fdButton.triggered.connect(self.fdLoadFlow)
+        loadFlowMenu.addAction(fdButton)
+        loadFlowMenu.addSeparator()
+
+        # Layouts
+        layout = QHBoxLayout()
+        layout.addWidget(Color('red'), 1)
+        layout.addWidget(Color('orange'), 12)
+        layout.addWidget(Color('blue'), 3)
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def makeProject(self) -> None:
+        pass
+
+    def openProject(self) -> None:
+        pass
+
+    def saveProject(self) -> None:
+        pass
+
+    def openExample(self) -> None:
+        pass
+
+    def exitProgram(self) -> None:
+        pass
+
+    def gsLoadFlow(self) -> None:
+        pass
+
+    def nrLoadFlow(self) -> None:
+        pass
+
+    def fdLoadFlow(self) -> None:
+        pass
--- src/methods.py
+++ src/methods.py
@@ -1,1 +1,73 @@
-#   
+# Methods of load flow calculations 
+
+# Imports
+import pandapower as pp 
+from termcolor import colored
+
+# Creates the network
+class netMaker():
+    def __init__(self) -> None:
+        self.net = pp.create_empty_network()
+        self.buses = {}
+        self.tBuses = {}
+        self.trafos = {}
+        self.busCounter = 0
+        self.trafoBusCounter = 0
+        self.trafoCounter = 0
+
+    def addSlack(self, key: str, vmPu: float, vaDeg: float):
+        pp.create_ext_grid(self.net, self.buses[key], vm_pu = vmPu, va_degree = vaDeg)
+
+    def addBusBar(self, vnKv: float, name: str):
+        self.busCounter += 1
+        key = f'Bus{self.busCounter}'
+        self.buses[key] = pp.create_bus(self.net, vn_kv = vnKv, name = name, type = 'b')
+
+    def addTransBus(self, vnKv: float, name: str):
+        self.trafoBusCounter += 1
+        key = f'TBus{self.trafoBusCounter}'
+        self.tBuses[key] = pp.create_bus(self.net, vn_kv = vnKv,
+                                             name = name, type = 'n')
+
+    def addTransformer(self, busOne: str, busTwo: str, name: str,
+                       stdType: str = '25 MVA 110/20 kV'):
+        self.trafoCounter += 1
+        key = f'Transformer{self.trafoCounter}'
+        busOneKey = self.tBuses[busOne]
+        busTwoKey = self.tBuses[busTwo]
+        self.trafos[key] = pp.create_transformer(self.net, busOneKey, busTwoKey,
+                                                 name = name, std_type = stdType)
+
+    def log(self):
+        print(colored(
+        '''
+                                █   █▀█ █▀▀ █▀
+                                █▄▄ █▄█ █▄█ ▄█
+        '''
+        , 'light_blue'))
+        print(colored('-' * 80, 'red'))
+        print(colored('-> Buses:', 'light_blue'), '\n', self.buses)
+        print(colored('-> Tranfo Buses:', 'light_blue'), '\n', self.tBuses)
+        print(colored('-> Transformers:', 'light_blue'), '\n', self.trafos)
+        print(colored('-' * 80, 'red'))
+        print(colored('-> Bus Table:', 'light_blue'), '\n', self.net.bus)
+        print(colored('-' * 80, 'red'))
+        print(colored('-> External Grid Table:', 'light_blue'), '\n', self.net.ext_grid)
+        print(colored('-' * 80, 'red'))
+        # print('Transformer Table: \n', self.net.trafo)
+
+    def run(self, method: str):
+        pp.runpp(self.net, algorithm = method)
+        print(self.net.res_bus)
+
+nMaker = netMaker()
+nMaker.addBusBar(110, 'Bus1')
+nMaker.addBusBar(20, 'Bus2')
+nMaker.addBusBar(110, 'Bus3')
+nMaker.addTransBus(110, 'TBus1')
+nMaker.addTransBus(20, 'TBus2')
+nMaker.addTransformer('TBus1', 'TBus2', '110kV/20kV transformer',
+                      )
+nMaker.addSlack('Bus1', 1.02, 50)
+nMaker.log()
+nMaker.run('gs')
