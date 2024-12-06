f7f6bf3923070e2fc444b0edfbaca0c14fcb698a
Alireza Jalouli <154112092+itsalirezajalouli@users.noreply.github.com>
2024-12-06 20:30:40 +0330

    Enums for Windings and BusTypes

--- src/psa_components.py
+++ src/psa_components.py
@@ -1,15 +1,28 @@
 #   Components like Bus, Line and Network...
+from enum import Enum
+
+class BusType(Enum):
+    SLACK = 'slack'
+    PV = 'pv'
+    PQ = 'pq'
+
+class Winding(Enum):
+    DELTA = 'delta'
+    Y = 'Y'
+    GY = 'Grounded Y'
 
 class BusBar():
     def __init__(self,
+                 pos: tuple[float, float],
                  id: int,
                  name: str,
-                 bType: str,
+                 bType: BusType,
                  vMag: float,
                  vAng: float,
                  P: float, 
                  Q: float,
                  ) -> None:
+        self.pos = pos
         self.name = name # just for GUI display
         self.id = id
         self.bType = bType
@@ -20,8 +33,36 @@ class BusBar():
 
 class Transformer():
     def __init__(self,
+                 pos: tuple[float, float],
+                 id: int,
+                 name: str,
                  R: float,
                  X: float,
+                 a: float,
+                 vBase: float,
+                 winding: Winding,
                  ) -> None:
-       self.R = R 
-       self.X = X 
+        self.pos = pos
+        self.name = name # just for GUI display
+        self.id = id
+        self.R = R 
+        self.X = X 
+        self.a = a
+        self.vBase = vBase
+        self.winding = winding
+
+class Line():
+    def __init__(self,
+                 bus1id: int,
+                 bus2id: int,
+                 R: float,
+                 X: float,
+                 Length: float,
+                 vBase: float,
+                 ) -> None:
+        self.bus1id = bus1id
+        self.bus2id = bus2id
+        self.R = R
+        self.X = X
+        self.Length = Length
+        self.vBase = vBase
